fb50ea42e722ef35e690bd605292547f08ea5697
finrails <macacofinn@yahoo.com>
2022-06-29 17:39:46 -0400

    upt: final tweaks

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,10 +1,20 @@
 class KittensController < ApplicationController
   def index
     @kittens = Kitten.all
+
+    respond_to do |format|
+      format.json { render json: @kittens }
+      format.html
+    end
   end
 
   def show
     @kitten = Kitten.find(params[:id])
+
+    respond_to do |format|
+      format.json { render json: @kitten }
+      format.html
+    end
   end
 
   def new
@@ -12,6 +22,14 @@ class KittensController < ApplicationController
   end
 
   def create
+    @kitten = Kitten.new(kitten_params)
+
+    if @kitten.save
+      redirect_to root_path, notice: "You've created a new cat with success!"
+    else
+      flash.now[:alert] = "Something went wrong, please try do it again."
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
@@ -19,8 +37,28 @@ class KittensController < ApplicationController
   end
 
   def update
+    @kitten = Kitten.find(params[:id])
+
+    if @kitten.update(kitten_params)
+      redirect_to root_path, notice: "The kitten has been updated with success!"
+    else
+      flash.now[:alert] = "Something went wrong! Try do it again."
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @kitten = Kitten.find(params[:id])
+
+    if @kitten.destroy
+      redirect_to root_path, notice: "That kitten has been deleted with success."
+    end
+
+  end
+
+  private
+
+  def kitten_params
+    params.require(:kitten).permit(:name, :age, :cuteness, :softness)
   end
 end
