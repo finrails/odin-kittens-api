08b37ac7609a9e93284019f0a790f8e503b6bf17
finrails <macacofinn@yahoo.com>
2022-06-29 15:04:30 -0400

    add: all basic views and actions for kittens controller

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,2 +1,26 @@
 class KittensController < ApplicationController
+  def index
+    @kittens = Kitten.all
+  end
+
+  def show
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def new
+    @kitten = Kitten.new
+  end
+
+  def create
+  end
+
+  def edit
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
